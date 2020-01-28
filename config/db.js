66d3d1ed8801f0ac8f3adda4b198bb8b56e66d81
Abu Zaid <zaid.abuzaid707@gmail.com>
2020-01-28 22:39:11 +0530

    user register

--- config/db.js
+++ config/db.js
@@ -1,20 +1,21 @@
-const mongoose = require('mongoose')
-const config = require('config')
-const db = config.get('mongoURI');
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(db, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log('MongoDB Connected.....')
-    } catch (err) {
-        console.error(err.Message)
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      createIndexes: true
+    });
+    console.log("MongoDB Connected.....");
+  } catch (err) {
+    console.error(err.Message);
 
-        // Exit process with faliure
-        process.exit(1)
-    }
-}
+    // Exit process with faliure
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
